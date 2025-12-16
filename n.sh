d30e1b61610ee85952907a74983d25871ba423e1
fscarmen <62703343+fscarmen@users.noreply.github.com>
2025-12-16 20:31:38 +0800

    Update n.sh

--- n.sh
+++ n.sh
@@ -5,15 +5,15 @@ CURL_ARGS=$2
 UA_Browser="Mozilla/5.0 (Windows NT 10.0; Win64; x6*4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36"
 
 MediaUnlockTest_Netflix() {
-  local RESULT_1=$(curl ${CURL_ARGS} ${MODE} --user-agent "${UA_Browser}" -SsL --max-time 10 --tlsv1.3 "https://www.netflix.com/title/81280792" 2>&1 | grep -E 'curl:|og:video|requestCountry')
+  local RESULT_1=$(curl ${CURL_ARGS} ${MODE} --user-agent "${UA_Browser}" -SsL --max-time 10 --tlsv1.3 "https://www.netflix.com/title/81280792" 2>&1 | awk '/curl:/{print}/og:video/{print "og:video"}{while(match($0,/"requestCountry":\{"supportedLocales":\[[^]]+\],"id":"([^"]+)"/,m)){c++;if(c==2){print "requestCountry:",m[1]}$0=substr($0,RSTART+RLENGTH)}}')
 
   grep -q 'curl:' <<< "$RESULT_1" && return 2
 
-  local RESULT_2=$(curl ${CURL_ARGS} ${MODE} --user-agent "${UA_Browser}" -SsL --max-time 10 --tlsv1.3 "https://www.netflix.com/title/70143836" 2>&1 | grep -E 'curl:|og:video|requestCountry')
+  local RESULT_2=$(curl ${CURL_ARGS} ${MODE} --user-agent "${UA_Browser}" -SsL --max-time 10 --tlsv1.3 "https://www.netflix.com/title/70143836" 2>&1 | awk '/curl:/{print}/og:video/{print "og:video"}{while(match($0,/"requestCountry":\{"supportedLocales":\[[^]]+\],"id":"([^"]+)"/,m)){c++;if(c==2){print "requestCountry:",m[1]}$0=substr($0,RSTART+RLENGTH)}}')
 
   grep -q 'curl:' <<< "$RESULT_2" && return 2
 
-  REGION_1=$(awk '{while(match($0,/"requestCountry":\{"supportedLocales":\[[^]]+\],"id":"[^"]+"/)){c++;s=substr($0,RSTART,RLENGTH);sub(/.*"id":"*/,"",s);sub(/".*/,"",s);if(c==2){print s;exit}$0=substr($0,RSTART+RLENGTH)}}' <<< "$RESULT_1")
+  REGION_1=$(awk '/requestCountry/{print $NF}' <<< "$RESULT_1")
 
   grep -q 'og:video' <<< "${RESULT_1}${RESULT_2}" && return 0 || return 1
 }
@@ -21,7 +21,7 @@ MediaUnlockTest_Netflix() {
 MediaUnlockTest_Netflix
 
 case "$?" in
-  0 ) echo -n -e "\r Netflix: Yes (Region: ${REGION_1})\n" ;;
-  1 ) echo -n -e "\r Netflix: Originals Only (Region: ${REGION_1})\n" ;;
+  0 ) echo -n -e "\r Netflix: Yes${REGION_1:+ (Region: ${REGION_1})}\n" ;;
+  1 ) echo -n -e "\r Netflix: Originals Only${REGION_1:+ (Region: ${REGION_1})}\n" ;;
   * ) echo -n -e "\r Netflix: Failed\n"
 esac
